f0a7b4479bc9d54acc050ef8873d5f74019fbdba
raushan kumar <142259385+styloraushan@users.noreply.github.com>
2025-11-08 10:44:44 +0530

    Update init_dataset.sh

--- init_dataset.sh
+++ init_dataset.sh
@@ -1,18 +1,11 @@
-#!/bin/bash
 set -e
-
 echo "Starting container initialization..."
-
-# If mounted /app/Dataset is empty, copy from backup
-
 if [ ! -f "/app/Dataset/diseasesymp_updated.csv" ]; then
   echo " Initializing dataset volume from backup..."
-
   mkdir -p /app/Dataset
   cp -r /app/Dataset_backup/* /app/Dataset/ 2>/dev/null || true
 else
   echo "Existing dataset detected — skipping initialization."
 fi
-
 echo "Launching Flask API..."
 python app.py
